d20dd9b33c4d05891b095d7ae915d8370ceb4691
Ashish Karn <ashishkrkarn@gmail.com>
2024-09-07 18:19:36 +0530

    Thread states

--- src/main/java/karn/core/multithreading/ThreadStates.java
+++ src/main/java/karn/core/multithreading/ThreadStates.java
@@ -1,4 +1,23 @@
 package karn.core.multithreading;
 
 public class ThreadStates {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread  = new Thread(()->{
+            System.out.println("RUNNING");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        System.out.println(thread.getState());
+        thread.start();
+        System.out.println(thread.getState());
+        Thread.sleep(100);
+        System.out.println(thread.getState());
+        thread.join();
+        System.out.println(thread.getState());
+    }
+
 }
